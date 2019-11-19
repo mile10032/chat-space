0cc538b214a0fc2efea8bba810c6793d0e18c052
mile10032 <57488122+mile10032@users.noreply.github.com>
2019-11-19 16:07:08 +0900

    tuikashita
    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles
-  root to: "messages#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index]
+  end
+  
 end
