cf1856e94a1efadbaded40413e702d58e7629197
mile10032 <57488122+mile10032@users.noreply.github.com>
2019-11-27 03:39:32 +0900

    自動更新の実装
    #what
    何秒かおきに、JavaScriptを使ってブラウザに表示されているメッセージのうち最も新しいもののidをリクエストとして送る
    JavaScriptを使って、レスポンスに含まれるメッセージたちをメッセージ一覧の最後に追加する
    #why
    いちいちリロードしなくても相手からのメッセージが表示される

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function(){
+
     function buildHTML(message){
       message.image ? image= `<img src=${message.image} >`: image = '';
       
@@ -21,28 +22,53 @@ $(function(){
           </div>`
           return html;
       }
-  $('.new_message').on('submit', function(e){
-    e.preventDefault()
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+      $('.new_message').on('submit', function(e){
+        e.preventDefault()
+        var formData = new FormData(this);
+        var url = $(this).attr('action')
+        $.ajax({
+          url: url,
+          type: "POST",
+          data: formData,
+          dataType: 'json',
+          processData: false,
+          contentType: false
+        })
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.messages').append(html);
+          $('.messages').animate({scrollTop:  $('.messages')[0].scrollHeight}, 'fast');
+          $('.form__submit').prop("disabled", false);
+          $('form')[0].reset();
+        })
+        .fail(function(){
+          alert('error');
+        });
+        return false;
+      });
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      last_message_id = $('.message:last').data("message-id")
+    $.ajax({ 
+      url: "api/messages",
+      type: 'get', 
+      dataType: 'json', 
+      data: {last_id: last_message_id}
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop:  $('.messages')[0].scrollHeight}, 'fast');
-      $('.form__submit').prop("disabled", false);
-      $('form')[0].reset();
+    .done(function (messages) { 
+      var insertHTML = '';
+      messages.forEach(function (message) {
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
+      })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
-    .fail(function(){
-      alert('error');
+    .fail(function () {
+      alert('自動更新に失敗しました');
+        });
+      }
+    };
+    $(function() {
+    setInterval(reloadMessages, 7000);
     });
-    return false;
-  });
 });
